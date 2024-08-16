7b7cf60283e256fbce06505d91d18363968ac3c3
himanishpuri <himanishpuri2203@gmail.com>
2024-08-16 09:55:26 +0530

    Update Tailwind CSS configuration and dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	prefix: "tw-", // This will change the default prefix from `` to `tw-`
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
